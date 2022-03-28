1ff1da6e88213514c2823425efa0ae9489f937d2
Imran189 <imranimu189@gmail.com>
2022-03-28 16:36:20 +0600

    initial commit

---
+++ src/components/AllComponent.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        <CounterPage/>
+        <HistoryPage/>
+    </div>
+</template>
+<script>
+import CounterPage from "./CounterPage.vue"
+import HistoryPage from "./HistoryPage.vue"
+export default {
+    components: {
+        CounterPage,
+        HistoryPage
+    },
+}
+</script>
+<style lang="">
+    
+</style>
