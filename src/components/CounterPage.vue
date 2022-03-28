0f39bf26751790f953d0643cdebd77fed5480512
Imran189 <imranimu189@gmail.com>
2022-03-28 16:35:50 +0600

    initial commit

---
+++ src/components/CounterPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Vuex Counter</h1>
+        <h1 class="counter">0</h1>
+        <button>-</button>
+        <input type="number">
+        <button>+</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .counter{
+       font-size: 3rem; 
+    }
+    button{
+        border-radius: 100%;
+        border: none;
+        width: 2rem;
+        height: 2rem;
+        font-weight: 700;
+        cursor: pointer;
+    }
+    input{
+        
+    }
+</style>
